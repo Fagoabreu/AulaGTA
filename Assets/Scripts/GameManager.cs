67197e3e52967555f2a20e8accb6e98c712bc0f4
Fagoabreu <noisabreus@hotmail.com>
2018-11-19 00:27:54 -0200

    Aula33
    Metade da aula 34, 7:55

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    #region HP
+    [SerializeField] Image hpImage;
+    [SerializeField] float curHealth;
+    [SerializeField] float maxHeath;
+
+    #endregion
+
+
+    [SerializeField] GameObject dieShowText;
+    // Use this for initialization
+    void Start () {
+        curHealth = maxHeath;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HealthChage();
+
+    }
+    void HealthChage()
+    {
+        float amount = curHealth / maxHeath;
+        hpImage.fillAmount = amount;
+
+        if(curHealth <= 0)
+        {
+            dieShowText.SetActive(true);
+        }
+        else
+        {
+            dieShowText.SetActive(false);
+        }
+    }
+}
